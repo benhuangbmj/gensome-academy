6d44993ea7facc0ac0a2e262615c921be931c4ad
Ben Huang <benjaminwong1985@gmail.com>
2026-01-07 17:24:48 -0500

    prototype: build the walking back and forth feature

--- src/scenes/main/callback.js
+++ src/scenes/main/callback.js
@@ -11,7 +11,7 @@ export default function gensomeAcademy() {
     const addProgress = obj.onUpdate(() => {
       if (obj.width > 0) {
         obj.use(
-          progress(10, {
+          progress(5, {
             width: obj.width,
             height: Math.max(obj.width / 10, 10),
             offset: vec2(0, 15).add(vec2(0, obj.height)),
@@ -24,7 +24,40 @@ export default function gensomeAcademy() {
         addProgress.cancel();
       }
     });
+    backNForth(obj, "right");
   });
-  add([sprite("girl", { anim: "down", width: 100 }), pos(center()), "girl"]);
+  const girl = add([sprite("girl", { width: 100 }), pos(center()), "girl"]);
   utilsScene.saveGame({ user });
+}
+
+function backNForth(obj, direction) {
+  if (direction == "right") {
+    const motion = obj.onUpdate(() => {
+      obj.moveTo(center().add(vec2(200, 0)), 50);
+      if (
+        JSON.stringify(obj.pos) === JSON.stringify(center().add(vec2(200, 0)))
+      ) {
+        motion.cancel();
+        obj.play("down");
+        wait(3, () => {
+          backNForth(obj, "left");
+        });
+      }
+    });
+    obj.play("right");
+  } else if (direction == "left") {
+    const motion = obj.onUpdate(() => {
+      obj.moveTo(center().add(vec2(-200, 0)), 50);
+      if (
+        JSON.stringify(obj.pos) === JSON.stringify(center().add(vec2(-200, 0)))
+      ) {
+        motion.cancel();
+        obj.play("down");
+        wait(3, () => {
+          backNForth(obj, "right");
+        });
+      }
+    });
+    obj.play("left");
+  }
 }
