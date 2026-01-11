721ed84442cfca1adcfb6721a58b1e7b26a8b70e
Ben Huang <benjaminwong1985@gmail.com>
2026-01-10 22:35:53 -0500

    fix: fix the undefined value issue for data structure modification

--- src/scenes/main/utils/generateUser.js
+++ src/scenes/main/utils/generateUser.js
@@ -1,6 +1,18 @@
-const defaultValue = { cash: 0, FP: 0, MP: 0, skills: {}, gameTime: 0 };
+const defaultValue = {
+  cash: 0,
+  FP: 0,
+  MP: 0,
+  skills: {},
+  gameTime: 0,
+  reputation: 0,
+};
 export default function generateUser() {
   const userData = getData("user-data", JSON.stringify(defaultValue));
   const user = userData ? JSON.parse(userData) : defaultValue;
+  for (let key in defaultValue) {
+    if (!(key in user)) {
+      user[key] = defaultValue[key];
+    }
+  }
   return user;
 }
