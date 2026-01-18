08aa83dfc4cfa4e0fef73aad41a4a420a3955f88
Ben Huang <benjaminwong1985@gmail.com>
2026-01-18 15:37:01 -0500

    change: remove the test character

--- src/scenes/main/level.js
+++ src/scenes/main/level.js
@@ -1,6 +1,4 @@
-import utils from "/src/utils.js";
 export default function makeMainLevel({ TILE_WIDTH, TILE_HEIGHT }) {
-  debug.log("Making main level", time());
   const levelMap = [
     "----------------",
     "|              |",
@@ -45,25 +43,6 @@ export default function makeMainLevel({ TILE_WIDTH, TILE_HEIGHT }) {
       ],
       vec2(5, 5),
     );
-    const girl = mainLevelComp.spawn(
-      [
-        sprite("girl", { frame: 0, width: TILE_WIDTH }),
-        anchor("center"),
-        pos(TILE_WIDTH / 2, TILE_HEIGHT / 2),
-        agent({ speed: 300, allowDiagonals: false }),
-      ],
-      vec2(15, 7),
-    );
-    girl.play("left");
-    girl.setTarget(mainLevelComp.tile2Pos(vec2(6, 4)));
-    let currDirection;
-    const updateAnim = girl.onUpdate(() => {
-      currDirection = utils.playDirectionAnim({
-        character: girl,
-        currDirection,
-        eventController: updateAnim,
-      });
-    });
   });
   return add([mainLevelComp, pos(0, 80), "main-level"]);
 }
