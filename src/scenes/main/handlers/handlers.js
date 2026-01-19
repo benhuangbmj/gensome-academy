338d31383d279ac1eb635ea04999cfc4240de276
Ben Huang <benjaminwong1985@gmail.com>
2026-01-19 11:22:12 -0500

    chore: add a handler to the export

--- src/scenes/main/handlers/handlers.js
+++ src/scenes/main/handlers/handlers.js
@@ -1,6 +1,8 @@
 import studentAdded from "./studentAdded";
 import mouseMoved from "./mouseMoved";
+import mousePressedAddItem from "./mousePressedAddItem";
 export default {
   studentAdded,
   mouseMoved,
+  mousePressedAddItem,
 };
