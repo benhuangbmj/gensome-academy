1ea9b0e9696f76f418c1e9663ff39bf1b93d6a05
Ben Huang <benjaminwong1985@gmail.com>
2026-01-20 13:33:13 -0500

    prototype: add the open menu function

--- src/scenes/main/gameObjs/UI/functionBtns.js
+++ src/scenes/main/gameObjs/UI/functionBtns.js
@@ -9,29 +9,58 @@ export default function functionBtns(user) {
   const SCALE = 1.5;
   const BTN_WIDTH = UISpecs.TILE_WIDTH * SCALE;
   const BTN_HEIGHT = UISpecs.TILE_HEIGHT * SCALE;
+  let menuOpened = false;
+  let drawMenuEvent;
   //const TEXT_SIZE = BTN_HEIGHT / 6;
   btnData.forEach((btn, i) => {
-    add([
+    const btnContainer = add([
       rect(BTN_WIDTH, BTN_HEIGHT),
       pos(
         width() - BTN_WIDTH - UISpecs.GAP,
         height() - (i + 1) * BTN_HEIGHT - UISpecs.GAP,
       ),
+      area(),
       fixed(),
       outline(4),
-    ])
-      .add([])
-      .add([
-        sprite(btn.sprite, { width: UISpecs.TILE_WIDTH }),
-        pos(
-          (BTN_WIDTH - UISpecs.TILE_WIDTH) / 2,
-          (BTN_HEIGHT - UISpecs.TILE_HEIGHT) / 2,
-        ),
-      ]);
+    ]);
+    btnContainer.add([
+      sprite(btn.sprite, { width: UISpecs.TILE_WIDTH }),
+      pos(
+        (BTN_WIDTH - UISpecs.TILE_WIDTH) / 2,
+        (BTN_HEIGHT - UISpecs.TILE_HEIGHT) / 2,
+      ),
+      area(),
+    ]);
+    btnContainer.onClick(() => {
+      //Bug: fixed component not working with onClick unless camera is moved away.
+      openMenu();
+    });
     //   .add([
     //     text(btn.tag, { size: TEXT_SIZE }),
     //     color(BLACK),
     //     pos((BTN_WIDTH - btn.tag.length * 7) / 2, (BTN_HEIGHT - TEXT_SIZE) / 2),
     //   ]);
   });
+  function openMenu() {
+    if (menuOpened) {
+      menuOpened = false;
+      drawMenuEvent.cancel();
+      return;
+    } else {
+      menuOpened = true;
+      drawMenuEvent = onDraw(() => {
+        drawRect({
+          width: 9 * BTN_WIDTH,
+          height: 3 * BTN_HEIGHT,
+          anchor: "botright",
+          fixed: true,
+          outline: { width: 4, color: BLACK },
+          pos: vec2(
+            width() - BTN_WIDTH - UISpecs.GAP - 4,
+            height() - UISpecs.GAP,
+          ),
+        });
+      });
+    }
+  }
 }
