75d763159733b6667b3e1fb448be840c61cbc2ab
Ben Huang <benjaminwong1985@gmail.com>
2026-01-28 08:44:59 -0500

    chore: clean up load sprites

--- src/scenes/main/utils/loadSprites.js
+++ src/scenes/main/utils/loadSprites.js
@@ -14,28 +14,8 @@ export default function loadSprites() {
   });
   loadSprite("copy-machine", "sprites/Copy Machine.png");
   loadSprite("gear", "sprites/gear.png");
-  loadSprite("girl", "sprites/girl.png", {
-    sliceX: 4,
-    sliceY: 5,
-    anims: {
-      down: { from: 12, to: 15, speed: 8, loop: true },
-      left: { from: 8, to: 11, speed: 8, loop: true },
-      right: { from: 4, to: 7, speed: 8, loop: true },
-      up: { from: 16, to: 19, speed: 8, loop: true },
-    },
-  });
-  loadSprite("julia", "sprites/julia.png", {
-    sliceX: 4,
-    sliceY: 4,
-    anims: {
-      down: { from: 0, to: 3, speed: 8, loop: true },
-      left: { from: 4, to: 7, speed: 8, loop: true },
-      right: { from: 8, to: 11, speed: 8, loop: true },
-      up: { from: 12, to: 15, speed: 8, loop: true },
-    },
-  });
+
   loadSprite("tropyhy", "sprites/trophy.png");
-  loadSprite("steel", "sprites/steel.png");
   loadSprite("floor", "sprites/tilestonebricks.png");
   loadCrew("sprite", "sparkling");
   loadCrew("sprite", "paper");
@@ -43,4 +23,26 @@ export default function loadSprites() {
   loadCrew("sprite", "pencil");
   loadCrew("sprite", "play");
   loadCrew("sprite", "wizarding");
+  loadCrew("sprite", "onion");
+  loadCrew("sprite", "steel");
+  // loadSprite("girl", "sprites/girl.png", {
+  //   sliceX: 4,
+  //   sliceY: 5,
+  //   anims: {
+  //     down: { from: 12, to: 15, speed: 8, loop: true },
+  //     left: { from: 8, to: 11, speed: 8, loop: true },
+  //     right: { from: 4, to: 7, speed: 8, loop: true },
+  //     up: { from: 16, to: 19, speed: 8, loop: true },
+  //   },
+  // });
+  // loadSprite("julia", "sprites/julia.png", {
+  //   sliceX: 4,
+  //   sliceY: 4,
+  //   anims: {
+  //     down: { from: 0, to: 3, speed: 8, loop: true },
+  //     left: { from: 4, to: 7, speed: 8, loop: true },
+  //     right: { from: 8, to: 11, speed: 8, loop: true },
+  //     up: { from: 12, to: 15, speed: 8, loop: true },
+  //   },
+  // });
 }
