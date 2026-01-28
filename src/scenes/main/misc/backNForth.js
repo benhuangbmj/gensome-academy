acf5ccb429b396ebcff6175919bdeb80b0efcf46
Ben Huang <benjaminwong1985@gmail.com>
2026-01-28 16:42:17 -0500

    refactor: improve robustness

--- src/scenes/main/misc/backNForth.js
+++ src/scenes/main/misc/backNForth.js
@@ -1,35 +1,41 @@
-const APLITUDE = 20;
 export default function backNForth(obj, direction, center) {
+  const APLITUDE = obj.width;
+  const SPEED = 2 * obj.width;
   center = center ?? vec2(...Object.values(obj.pos));
   if (direction == "right") {
     const motion = obj.onUpdate(() => {
-      obj.moveTo(center.add(vec2(APLITUDE, 0)), 50);
+      obj.moveTo(center.add(vec2(APLITUDE, 0)), SPEED);
       if (
         JSON.stringify(obj.pos) ===
         JSON.stringify(center.add(vec2(APLITUDE, 0)))
       ) {
         motion.cancel();
-        obj.play("down");
+        playAnim(obj, "down");
         wait(3, () => {
           backNForth(obj, "left", center);
         });
       }
     });
-    obj.play("right");
+    playAnim(obj, "right");
   } else if (direction == "left") {
     const motion = obj.onUpdate(() => {
-      obj.moveTo(center.add(vec2(-APLITUDE, 0)), 50);
+      obj.moveTo(center.add(vec2(-APLITUDE, 0)), SPEED);
       if (
         JSON.stringify(obj.pos) ===
         JSON.stringify(center.add(vec2(-APLITUDE, 0)))
       ) {
         motion.cancel();
-        obj.play("down");
+        playAnim(obj, "down");
         wait(3, () => {
           backNForth(obj, "right", center);
         });
       }
     });
-    obj.play("left");
+    playAnim(obj, "left");
+  }
+}
+function playAnim(obj, anim) {
+  if (obj.hasAnim(anim)) {
+    obj.play(anim);
   }
 }
