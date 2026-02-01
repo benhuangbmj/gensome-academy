633ff981cc62eb987dbae6f55da80d9fa4711cb3
Ben Huang <benjaminwong1985@gmail.com>
2026-02-01 14:26:39 -0500

    fix: correct the enrolling logic

--- src/scenes/main/utils/enroll.js
+++ src/scenes/main/utils/enroll.js
@@ -1,6 +1,14 @@
 import userContext from "../contexts/userContext";
 import encode from "./encode";
-export default function enroll(student) {
+export default function enroll(student, isReenrolled = false) {
+  if (!student) {
+    user.enrolled--;
+    return;
+  }
   const user = userContext.provide();
+  console.log("roster", user.roster);
   user.roster.push(encode(student));
+  if (!isReenrolled) {
+    user.enrolled++;
+  }
 }
--- src/scenes/main/utils/isEnrollmentFull.js
+++ src/scenes/main/utils/isEnrollmentFull.js
@@ -2,11 +2,7 @@ import userContext from "../contexts/userContext";
 import config from "../../../config";
 export default function isEnrollmentFull() {
   const user = userContext.provide();
-  if (
-    user.enrolled >=
-    config.ROSTER_BASELINE + Math.floor(user.reputation / 100)
-  )
-    return false;
-  user.enrolled++;
-  return true;
+  return (
+    user.enrolled >= config.ROSTER_BASELINE + Math.floor(user.reputation / 100)
+  );
 }
