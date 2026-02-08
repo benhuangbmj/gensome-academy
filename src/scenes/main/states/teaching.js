679f9db98e2384c256fc161b664f9771ad593ce4
Ben Huang <benjaminwong1985@gmail.com>
2026-02-08 15:09:05 -0500

    change: rewrite the activity logic

--- src/scenes/main/states/teaching.js
+++ src/scenes/main/states/teaching.js
@@ -1,51 +1,22 @@
-import config from "../../../config";
 import activity from "../../../components/activity";
 import progress from "../../../components/progress";
-import scheduleNext from "../utils/scheduleNext";
-import enroll from "../utils/enroll";
-import userContext from "../contexts/userContext";
-export default function teaching(tutor, student) {
-  const duration = 50 * config.TIME_FLOW_RATE;
-  tutor.workerUsage++;
-  student.add([
+import config from "../../../config";
+export default function teaching(tutor) {
+  const duration = Infinity;
+  tutor.add([
     pos(),
-    progress(duration, {
-      width: student.width,
-    }),
+    progress(duration),
     activity({
       actor: tutor,
-      target: student,
-      type: "teach",
-      effect: (actor, target) => {
-        actor.workerUsage--;
-        if (actor.workerUsage === 0) {
-          actor.enterState("idle");
-        }
-        target.enterState("leaving", tutor, student);
-        scheduleNext();
-        releaseCheck(actor, target);
+      type: "teaching",
+      ongoing: (actor) => {
+        actor.workerUsage.forEach((student) => {
+          student.customerPerformance +=
+            ((actor.workerEfficiency * 20) / (50 * config.TIME_FLOW_RATE)) *
+            dt();
+        });
       },
     }),
+    "teaching",
   ]);
 }
-function releaseCheck(tutor, student) {
-  const user = userContext.provide();
-  user.attending.shift();
-  student.customerPerformance += 20 * tutor.workerEfficiency;
-  student.addAttendance();
-  if (student.customerAttendance >= 5) {
-    updateReputation(student);
-    enroll();
-  } else {
-    enroll(student, student.customerIsReturning);
-  }
-}
-
-function updateReputation(student) {
-  const user = userContext.provide();
-  user.reputation = Math.ceil(
-    (user.reputation * (user.enrolled - 1) + student.customerPerformance) /
-      user.enrolled,
-  );
-}
-//TODO: separate teaching from learning
