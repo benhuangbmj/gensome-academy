3f697e2c1eb34ddd1a6070825321c199deb722aa
Ben Huang <benjaminwong1985@gmail.com>
2026-02-16 15:01:44 -0500

    change

--- src/scenes/main/handlers/studentAdded.js
+++ src/scenes/main/handlers/studentAdded.js
@@ -1,9 +1,10 @@
 import findSecretary from "../utils/findSecretary";
 import moveToSec from "../utils/moveToSec";
+import config from "../../../config";
 export default function studentAdded(obj) {
   const loopController = findSecretary(obj, "check-in", [obj], moveToSec(obj));
   if (!obj.customerIsReturning)
-    wait(30, () => {
+    wait(30 * config.TIME_FLOW_RATE, () => {
       if (obj.state === "idle") {
         loopController.cancel();
         obj.enterState("dismissed", obj);
