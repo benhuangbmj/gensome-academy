a72d7196127918aefe9dfea690c0fb88267742e4
Ben Huang <benjaminwong1985@gmail.com>
2026-02-15 21:18:00 -0500

    chore

--- src/scenes/main/contexts/dataContext.js
+++ src/scenes/main/contexts/dataContext.js
@@ -1,9 +1,11 @@
 import Papa from "papaparse";
 import facilitiesCSV from "../data/facilities.csv?raw";
+import practicesCSV from "../data/practices.csv?raw";
 
 const facilities = Papa.parse(facilitiesCSV, { header: true }).data;
+const practices = Papa.parse(practicesCSV, { header: true }).data;
 function provide() {
-  return { facilities };
+  return { facilities, practices };
 }
 
 export default {
