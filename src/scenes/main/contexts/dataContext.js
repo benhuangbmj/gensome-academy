0a71a3b9b7085a82ec6e0e146721dadd31959461
Ben Huang <benjaminwong1985@gmail.com>
2026-02-16 14:41:25 -0500

    change: use dynamic typing in parsing

--- src/scenes/main/contexts/dataContext.js
+++ src/scenes/main/contexts/dataContext.js
@@ -1,11 +1,22 @@
 import Papa from "papaparse";
 import facilitiesCSV from "../data/facilities.csv?raw";
 import practicesCSV from "../data/practices.csv?raw";
+import personnelCSV from "../data/personnel.csv?raw";
 
-const facilities = Papa.parse(facilitiesCSV, { header: true }).data;
-const practices = Papa.parse(practicesCSV, { header: true }).data;
+const facilities = Papa.parse(facilitiesCSV, {
+  header: true,
+  dynamicTyping: true,
+}).data;
+const practices = Papa.parse(practicesCSV, {
+  header: true,
+  dynamicTyping: true,
+}).data;
+const personnel = Papa.parse(personnelCSV, {
+  header: true,
+  dynamicTyping: true,
+}).data;
 function provide() {
-  return { facilities, practices };
+  return { facilities, practices, personnel };
 }
 
 export default {
