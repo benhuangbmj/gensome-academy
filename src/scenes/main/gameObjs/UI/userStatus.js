740139ebe2b9fed5cd5cc48321e547696540f4a4
Ben Huang <benjaminwong1985@gmail.com>
2026-02-20 14:36:34 -0500

    refactor: replace the icon on click functions

--- src/scenes/main/gameObjs/UI/userStatus.js
+++ src/scenes/main/gameObjs/UI/userStatus.js
@@ -5,10 +5,10 @@ export default function generateUserStatus(user) {
   const TEXT_SIZE = 24;
   const ICON_HEIGHT = 36;
   const statusData = [
-  {sprite: {name: "coin", opt: {anim: "shine"}},text: () =>user.cash},
-  {sprite: {name: "controller", opt: {}}, text: () =>user.FP},
-  {sprite: {name: "config", opt: {}}, text: () =>user.MP},
-  {sprite: {name: "star", opt: {}}, text: () =>user.reputation},
+  {sprite: {name: "coin", opt: {anim: "shine"}},text: () =>user.cash, onClick: coinIconOnClick },
+  {sprite: {name: "controller", opt: {}}, text: () =>user.FP, onClick: facilityIconOnClick},
+  {sprite: {name: "config", opt: {}}, text: () =>user.MP, onClick: gearIconOnClick},
+  {sprite: {name: "star", opt: {}}, text: () =>user.reputation, onClick: starIconOnClick},
   {sprite: {name: "onion", opt:{}}, text: () =>null}
 ]
   const HEIGHT = (statusData.length + 1) * GAP + statusData.length * ICON_HEIGHT;
@@ -18,158 +18,17 @@ export default function generateUserStatus(user) {
     outline(4),
     fixed(),
   ]);
-  const coinIcon = userStatus.add([
-    sprite("coin", { anim: "shine", height: ICON_HEIGHT }),
-    pos(GAP, GAP),
-    area(),
-  ]);
-  const cashText = userStatus.add([
-    text(`${user.cash}`, { size: TEXT_SIZE }),
-    pos(userStatus.width - GAP, GAP + (ICON_HEIGHT - TEXT_SIZE) / 2),
-    color(BLACK),
-    anchor("topright"),
-    {add() {
-      this.hidden = true;
-    }}
-  ]);
-  cashText.onDraw(() => {
-    cashText.text = `${user.cash}`;
-  });
-
-  const facilityIcon = userStatus.add([
-    sprite("copy-machine", { height: ICON_HEIGHT + 8 }),
-    pos(GAP - 4, 2 * GAP + ICON_HEIGHT - 10),
-    area(),
-    {add() {
-      this.hidden = true;
-    }}
-  ]);
-  const FPText = userStatus.add([
-    text(`${user.FP}`, { size: TEXT_SIZE }),
-    pos(
-      userStatus.width - GAP,
-      2 * GAP + ICON_HEIGHT + (ICON_HEIGHT - TEXT_SIZE) / 2,
-    ),
-    color(BLACK),
-    anchor("topright"),
-    {add() {
-      this.hidden = true;
-    }}
-  ]);
-  FPText.onDraw(() => {
-    FPText.text = `${user.FP}`;
-  });
-
-  const gearIcon = userStatus.add([
-    sprite("gear", { height: ICON_HEIGHT }),
-    pos(GAP, 3 * GAP + 2 * ICON_HEIGHT),
-    area(),
-    {add() {
-      this.hidden = true;
-    }}
-  ]);
-  const MPText = userStatus.add([
-    text(`${user.MP}`, { size: TEXT_SIZE }),
-    pos(
-      userStatus.width - GAP,
-      3 * GAP + 2 * ICON_HEIGHT + (ICON_HEIGHT - TEXT_SIZE) / 2,
-    ),
-    color(BLACK),
-    anchor("topright"),
-    {add() {
-      this.hidden = true;
-    }}
-  ]);
-  MPText.onDraw(() => {
-    MPText.text = `${user.MP}`;
-  });
-
-  const trophyIcon = userStatus.add([
-    sprite("trophy", { height: ICON_HEIGHT }),
-    pos(GAP, 4 * GAP + 3 * ICON_HEIGHT),
-    area(),
-    {add() {
-      this.hidden = true;
-    }}
-  ]);
-  const reputationText = userStatus.add([
-    text(`${user.reputation}`, { size: TEXT_SIZE }),
-    pos(
-      userStatus.width - GAP,
-      4 * GAP + 3 * ICON_HEIGHT + (ICON_HEIGHT - TEXT_SIZE) / 2,
-    ),
-    color(BLACK),
-    anchor("topright"),
-    {add() {
-      this.hidden = true;
-    }}
-  ]);
-  reputationText.onDraw(() => {
-    reputationText.text = `${user.reputation}`;
-  });
-
-  function mouseClickedAction(cbLeft, cbRight) {
-    if (isMousePressed("left")) {
-      cbLeft();
-    }
-    if (isMousePressed("right")) {
-      cbRight();
-    }
-  }
-  if (environment === "dev") {
-    coinIcon.onClick(() => {
-      mouseClickedAction(
-        () => {
-          debug.log("Adding cash...");
-          user.cash += 1000;
-        },
-        () => {
-          debug.log("Subtracting cash...");
-          user.cash = Math.max(0, user.cash - 10000);
-        },
-      );
-    });
-    facilityIcon.onClick(() => {
-      mouseClickedAction(
-        () => {
-          debug.log("Adding FP...");
-          user.FP += 100;
-        },
-        () => {
-          debug.log("Subtracting FP...");
-          user.FP = Math.max(0, user.FP - 100);
-        },
-      );
-    });
-    gearIcon.onClick(() => {
-      mouseClickedAction(
-        () => {
-          debug.log("Adding MP...");
-          user.MP += 100;
-        },
-        () => {
-          debug.log("Subtracting MP...");
-          user.MP = Math.max(0, user.MP - 100);
-        },
-      );
-    });
-    trophyIcon.onClick(() => {
-      mouseClickedAction(
-        () => {
-          debug.log("Adding reputation...");
-          user.reputation += 10;
-        },
-        () => {
-          debug.log("Subtracting reputation...");
-          user.reputation = Math.max(0, user.reputation - 10);
-        },
-      );
-    });
-  }  
   statusData.forEach((data, i) => {
     userStatus.add([
       sprite(data.sprite.name, Object.assign(data.sprite.opt, {height: ICON_HEIGHT})),
-      pos(GAP, (i + 1) * GAP + i * ICON_HEIGHT)]);
+      pos(GAP, (i + 1) * GAP + i * ICON_HEIGHT),
+      area(), {
+        add() {
+           if (environment === "dev") {
+            data.onClick && this.onClick(data.onClick);
+           }
+        }
+      }]);
     userStatus.add([
       text(data.text() !== null ? `${data.text()}` : "", { size: TEXT_SIZE }),
       pos(
@@ -185,4 +44,61 @@ export default function generateUserStatus(user) {
       }
     ]);  
     })
+    
+  function mouseClickedAction(cbLeft, cbRight) {
+    if (isMousePressed("left")) {
+      cbLeft();
+    }
+    if (isMousePressed("right")) {
+      cbRight();
+    }
+  }
+  function coinIconOnClick(){
+      mouseClickedAction(
+        () => {
+          debug.log("Adding cash...");
+          user.cash += 1000;
+        },
+        () => {
+          debug.log("Subtracting cash...");
+          user.cash = Math.max(0, user.cash - 10000);
+        },
+      );
+    }
+    function facilityIconOnClick(){
+      mouseClickedAction(
+        () => {
+          debug.log("Adding FP...");
+          user.FP += 100;
+        },
+        () => {
+          debug.log("Subtracting FP...");
+          user.FP = Math.max(0, user.FP - 100);
+        },
+      );
+    }
+    function gearIconOnClick() {
+      mouseClickedAction(
+        () => {
+          debug.log("Adding MP...");
+          user.MP += 100;
+        },
+        () => {
+          debug.log("Subtracting MP...");
+          user.MP = Math.max(0, user.MP - 100);
+        },
+      );
+    }
+    function starIconOnClick() {
+      mouseClickedAction(
+        () => {
+          debug.log("Adding reputation...");
+          user.reputation += 10;
+        },
+        () => {
+          debug.log("Subtracting reputation...");
+          user.reputation = Math.max(0, user.reputation - 10);
+        },
+      );
+    }
 }
