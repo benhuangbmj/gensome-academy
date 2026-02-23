55302d41710cd12bcd9e3ec42ce4841069ff9ad6
Ben Huang <benjaminwong1985@gmail.com>
2026-02-23 10:18:24 -0500

    refactor

--- src/scenes/main/handlers/mousePressedAddItem.js
+++ src/scenes/main/handlers/mousePressedAddItem.js
@@ -1,11 +1,23 @@
 import levelContext from "../contexts/levelContext";
 import utils from "../../../utils";
 //TODO: fix the argument structure to be consistent with mouseMovedAddItem
-export default function mousePressedAddItem(btn, item, isApproved) {
+export default function mousePressedAddItem(
+  btn,
+  item,
+  isApproved,
+  callback = spawnItem,
+) {
   item.size = vec2(item.size_w, item.size_h);
   const level = levelContext.provide();
   if (!isApproved || btn != "left") return;
   const tilePos = level.adjustedPos2Tile(mousePos());
+  if (!callback) return;
+  callback(level, tilePos, item);
+  utils.makeBySize(tilePos, item.size, (tilePos) =>
+    spawnBluePrint(level, tilePos),
+  );
+}
+function spawnItem(level, tilePos, item) {
   level.spawn(
     [
       sprite(item.sprite, {
@@ -15,10 +27,7 @@ export default function mousePressedAddItem(btn, item, isApproved) {
     ],
     tilePos,
   );
-  utils.makeBySize(tilePos, item.size, (tilePos) =>
-    spawnBluePrint(level, tilePos, item),
-  );
 }
-function spawnBluePrint(level, tilePos, item) {
+function spawnBluePrint(level, tilePos) {
   level.spawn([tile({ isObstacle: true })], tilePos);
 }
