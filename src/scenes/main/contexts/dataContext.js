eb8de785396b7420022617ce41de688ee6cd7b45
Ben Huang <benjaminwong1985@gmail.com>
2026-02-23 11:37:58 -0500

    data

--- src/scenes/main/contexts/dataContext.js
+++ src/scenes/main/contexts/dataContext.js
@@ -2,6 +2,8 @@ import Papa from "papaparse";
 import facilitiesCSV from "../data/facilities.csv?raw";
 import practicesCSV from "../data/practices.csv?raw";
 import personnelCSV from "../data/personnel.csv?raw";
+import statusCSV from "../data/status.csv?raw";
+import facility_statusCSV from "../data/facility_status.csv?raw";
 
 const facilities = Papa.parse(facilitiesCSV, {
   header: true,
@@ -15,8 +17,16 @@ const personnel = Papa.parse(personnelCSV, {
   header: true,
   dynamicTyping: true,
 }).data;
+const status = Papa.parse(statusCSV, {
+  header: true,
+  dynamicTyping: true,
+}).data;
+const facilityStatus = Papa.parse(facility_statusCSV, {
+  header: true,
+  dynamicTyping: true,
+}).data;
 function provide() {
-  return { facilities, practices, personnel };
+  return { facilities, practices, personnel, status, facilityStatus };
 }
 
 export default {
