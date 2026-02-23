7a2190879caffdd3a6f3a4806752f0f30762fba5
Ben Huang <benjaminwong1985@gmail.com>
2026-02-23 11:38:28 -0500

    feat: add tags to the facility

--- src/scenes/main/handlers/mousePressedAddItem.js
+++ src/scenes/main/handlers/mousePressedAddItem.js
@@ -1,5 +1,6 @@
 import levelContext from "../contexts/levelContext";
 import utils from "../../../utils";
+import dataContext from "../contexts/dataContext";
 //TODO: fix the argument structure to be consistent with mouseMovedAddItem
 export default function mousePressedAddItem(
   btn,
@@ -24,10 +25,32 @@ function spawnItem(level, tilePos, item) {
         width: item.size_w * level.tileWidth(),
         height: item.size_h * level.tileHeight(),
       }),
+      {
+        add() {
+          console.log("tags", this.tags);
+        },
+      },
+      ...makeTags(item),
     ],
     tilePos,
   );
 }
 function spawnBluePrint(level, tilePos) {
   level.spawn([tile({ isObstacle: true })], tilePos);
+}
+function makeTags(facility) {
+  const facilityId = facility.id;
+  const data = dataContext.provide();
+  let isSite = false;
+  const statusIds = data.facilityStatus
+    .filter((record) => record.facility_id == facilityId)
+    .map((record) => {
+      record.site && (isSite = true);
+      return record.status_id;
+    });
+  const tags = data.status
+    .filter((record) => statusIds.includes(record.id))
+    .map((record) => record.name);
+  isSite && tags.push("site");
+  return tags;
 }
